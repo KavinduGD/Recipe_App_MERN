7a84666cae5e13ae3663302b03e93a8605f51a6e
KavinduGD <it21038914@my.sliit.lk>
2024-02-22 22:14:24 +0530

    ad

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,76 +33,319 @@ function Home() {
               </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div>
-            <div className="relative w-full pt-[100%]">
-              <img
-                src="https://foodtank.com/wp-content/uploads/2022/05/Food-As-Medicine-Featured.jpg"
-                alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover"
-              />
-            </div>
-            <div>bottom</div>
-          </div>
-        </div>
-        <div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
           <div>
             <div className="relative w-full pt-[100%]">
               <img
                 src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
                 alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
               />
             </div>
-            <div>bottom</div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
           <div>
             <div className="relative w-full pt-[100%]">
               <img
                 src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
                 alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
               />
             </div>
-            <div>bottom</div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
           <div>
             <div className="relative w-full pt-[100%]">
               <img
                 src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
                 alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
               />
             </div>
-            <div>bottom</div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
           <div>
             <div className="relative w-full pt-[100%]">
               <img
                 src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
                 alt=""
-                className="absolute top-0 left-0 w-full h-full object-cover"
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
               />
             </div>
-            <div>bottom</div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="sm rounded-md border-2 ">
+          <div>
+            <div className="relative w-full pt-[100%]">
+              <img
+                src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+                alt=""
+                className="absolute top-0 left-0 w-full h-full object-cover rounded-t-sm"
+              />
+            </div>
+            <div className="flex flex-col px-[5px] pb-[10px]">
+              <p className="font-crimsonPro text-[20px] flex-1]">
+                Pancake breakfast tacos
+              </p>
+              <div className="flex justify-between font-crimsonPro mt-2  pc-[10x]">
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Edit
+                </button>
+                <button className="px-[10px] py-[5px] bg-[#373538] text-white">
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
-        <div>a</div>
       </div>
     </div>
   );
