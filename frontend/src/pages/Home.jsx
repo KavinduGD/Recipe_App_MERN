3e16aeff6d6ccf3984130d82e3321909b15a69fe
KavinduGD <it21038914@my.sliit.lk>
2024-02-22 21:30:14 +0530

    grid working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,40 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="sm:grid sm:grid-cols-4">
+    <div className="sm:grid sm:grid-cols-4 gap-x-1">
       <div>
         <div>
-          <div>
+          <div className="relative w-full pt-[100%]">
             <img
               src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
               alt=""
-              className=" "
+              className="absolute top-0 left-0 w-full h-full object-cover"
+            />
+          </div>
+          <div>bottom</div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div className="relative w-full pt-[100%]">
+            <img
+              src="https://foodtank.com/wp-content/uploads/2022/05/Food-As-Medicine-Featured.jpg"
+              alt=""
+              className="absolute top-0 left-0 w-full h-full object-cover"
+            />
+          </div>
+          <div>bottom</div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div className="relative w-full pt-[100%]">
+            <img
+              src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+              alt=""
+              className="absolute top-0 left-0 w-full h-full object-cover"
             />
           </div>
           <div>bottom</div>
